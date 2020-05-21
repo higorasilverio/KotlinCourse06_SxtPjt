9eae85f7d3f111973dd7329a24dceeda0e34ca09
higorasilverio <higorasilverio@outlook.com>
2020-05-21 19:03:19 -0300

    App to start to work with layouts: vertical guideline - final

---
+++ app/src/main/java/br/com/silverio/kotlincourse06_sxtpjt/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.silverio.kotlincourse06_sxtpjt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
